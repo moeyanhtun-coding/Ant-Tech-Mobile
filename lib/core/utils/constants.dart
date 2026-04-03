6204a5364dbc23182e0bdba81d50fd7e72eadb58
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-03 21:08:51 +0630

    Add AppConstants class for API base URL and login endpoint configuration

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  // Use http://10.0.2.2:5000 for Android emulator
+  // Use http://localhost:5000 for iOS simulator or Web
+  static const String baseUrl = 'http://10.0.2.2:5000/api';
+  static const String loginEndpoint = '/AuthApi/login';
+}
