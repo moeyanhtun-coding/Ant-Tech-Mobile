0d1cf55bfd1ec73804ba28efc125beb1d1c4d4c6
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-03 21:12:52 +0630

    Add main application entry point with MultiBlocProvider for Auth and Home features, integrating SplashScreen as the initial route

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'features/home/presentation/bloc/home_bloc.dart';
+import 'features/splash/presentation/pages/splash_screen.dart';
+import 'injection_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => di.sl<AuthBloc>(),
+        ),
+        BlocProvider<HomeBloc>(
+          create: (_) => di.sl<HomeBloc>(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'HWD HR Mobile',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          useMaterial3: true,
+        ),
+        home: const SplashScreen(),
+      ),
+    );
+  }
+}
