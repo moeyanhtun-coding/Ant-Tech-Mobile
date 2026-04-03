e34ccfac3e41e7f1c94e98f81b2204b11dd42b97
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-03 21:10:45 +0630

    Add AuthState class and its subclasses to represent various authentication states

---
+++ lib/features/auth/presentation/bloc/auth_state.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import '../../domain/entities/user_entity.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthAuthenticated extends AuthState {
+  final UserEntity user;
+
+  const AuthAuthenticated({required this.user});
+
+  @override
+  List<Object?> get props => [user];
+}
+
+class AuthUnauthenticated extends AuthState {}
+
+class AuthFailure extends AuthState {
+  final String message;
+
+  const AuthFailure({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
