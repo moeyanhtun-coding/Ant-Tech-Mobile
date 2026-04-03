e7f7654b976647f17e69613145f4acf030e991f9
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-03 21:11:19 +0630

    Add HomeRepositoryImpl class to manage user profile retrieval with error handling

---
+++ lib/features/home/data/repositories/home_repository_impl.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/error/failures.dart';
+import '../../domain/entities/profile_entity.dart';
+import '../../domain/repositories/home_repository.dart';
+import '../datasources/home_remote_data_source.dart';
+
+class HomeRepositoryImpl implements HomeRepository {
+  final HomeRemoteDataSource remoteDataSource;
+
+  HomeRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<Either<Failure, ProfileEntity>> getProfile() async {
+    try {
+      final profile = await remoteDataSource.getProfile();
+      return Right(profile);
+    } on ServerFailure catch (e) {
+      return Left(e);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
