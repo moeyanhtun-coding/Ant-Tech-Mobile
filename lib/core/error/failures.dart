e113789813f783603f2a93c5e81120ee80ffcd0e
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-03 21:08:27 +0630

    Add error handling classes for failures in the application

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final String message;
+  const Failure([this.message = 'An unexpected error occurred']);
+
+  @override
+  List<Object> get props => [message];
+}
+
+// General failures
+class ServerFailure extends Failure {
+  const ServerFailure([super.message]);
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure([super.message]);
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure([super.message = 'No Internet Connection']);
+}
