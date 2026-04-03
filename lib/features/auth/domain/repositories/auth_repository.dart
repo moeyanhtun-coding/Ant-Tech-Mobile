9004df10f06e4ff15bb1b44a47acfadc646792b8
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-03 21:09:46 +0630

    Add AuthRepository interface for user login functionality with error handling

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/error/failures.dart';
+import '../entities/user_entity.dart';
+
+abstract class AuthRepository {
+  Future<Either<Failure, UserEntity>> login({
+    required String username,
+    required String password,
+  });
+}
