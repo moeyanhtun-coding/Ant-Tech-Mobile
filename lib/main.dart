1a498c6d0a1cd06b3a94b58dc54f7731156c58bb
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-04 00:40:14 +0630

    feat: integrate ThemeBloc into MyApp for dynamic theming based on user preferences

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'features/auth/presentation/bloc/auth_bloc.dart';
 import 'features/home/presentation/bloc/home_bloc.dart';
 import 'features/attendance/presentation/bloc/attendance_bloc.dart';
+import 'features/settings/presentation/bloc/theme_bloc.dart';
+import 'features/settings/presentation/bloc/theme_event.dart';
+import 'features/settings/presentation/bloc/theme_state.dart';
 import 'features/splash/presentation/pages/splash_screen.dart';
 import 'injection_container.dart' as di;
 
@@ -29,18 +32,43 @@ class MyApp extends StatelessWidget {
         BlocProvider<AttendanceBloc>(
           create: (_) => di.sl<AttendanceBloc>(),
         ),
-      ],
-      child: MaterialApp(
-        title: 'HWD HR Mobile',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          textTheme: GoogleFonts.poppinsTextTheme(
-            Theme.of(context).textTheme,
-          ),
-          useMaterial3: true,
+        BlocProvider<ThemeBloc>(
+          create: (_) => di.sl<ThemeBloc>()..add(GetInitialTheme()),
         ),
-        home: const SplashScreen(),
+      ],
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, state) {
+          return MaterialApp(
+            title: 'HWD HR Mobile',
+            debugShowCheckedModeBanner: false,
+            themeMode: state.themeMode,
+            theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: const Color(0xFF2193b0),
+                primary: const Color(0xFF2193b0),
+                surface: const Color(0xFFF5F7FB),
+              ),
+              textTheme: GoogleFonts.poppinsTextTheme(
+                Theme.of(context).textTheme,
+              ),
+            ),
+            darkTheme: ThemeData(
+              useMaterial3: true,
+              brightness: Brightness.dark,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: const Color(0xFF4facfe),
+                brightness: Brightness.dark,
+                surface: const Color(0xFF0F172A),
+                primary: const Color(0xFF38BDF8),
+              ),
+              textTheme: GoogleFonts.poppinsTextTheme(
+                ThemeData.dark().textTheme,
+              ),
+            ),
+            home: const SplashScreen(),
+          );
+        },
       ),
     );
   }
