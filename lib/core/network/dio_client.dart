02098ebebc52df2d6a6bca0e9bb7e4299ccf5374
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-03 21:08:35 +0630

    Add DioClient class for network requests with configuration and logging

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import '../utils/constants.dart';
+
+class DioClient {
+  final Dio dio;
+
+  DioClient(this.dio) {
+    dio.options = BaseOptions(
+      baseUrl: AppConstants.baseUrl,
+      connectTimeout: const Duration(seconds: 15),
+      receiveTimeout: const Duration(seconds: 15),
+      contentType: 'application/json',
+    );
+
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+      logPrint: (obj) => print(obj),
+    ));
+  }
+}
