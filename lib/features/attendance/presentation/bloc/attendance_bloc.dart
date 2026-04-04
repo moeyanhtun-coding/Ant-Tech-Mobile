f853b59bc9612479cd47091c8ca04761ebc64aab
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-04 11:19:04 +0630

    feat: implement ScanQRCodeUseCase for handling QR code scanning logic in attendance feature

--- lib/features/attendance/presentation/bloc/attendance_bloc.dart
+++ lib/features/attendance/presentation/bloc/attendance_bloc.dart
@@ -1,13 +1,19 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../domain/usecases/get_attendance_usecase.dart';
+import '../../domain/usecases/scan_qr_code_usecase.dart';
 import 'attendance_event.dart';
 import 'attendance_state.dart';
 
 class AttendanceBloc extends Bloc<AttendanceEvent, AttendanceState> {
   final GetAttendanceUseCase getAttendanceUseCase;
+  final ScanQRCodeUseCase scanQRCodeUseCase;
 
-  AttendanceBloc({required this.getAttendanceUseCase}) : super(AttendanceInitial()) {
+  AttendanceBloc({
+    required this.getAttendanceUseCase,
+    required this.scanQRCodeUseCase,
+  }) : super(AttendanceInitial()) {
     on<GetAttendanceRequested>(_onGetAttendanceRequested);
+    on<ScanQRCodeRequested>(_onScanQRCodeRequested);
   }
 
   Future<void> _onGetAttendanceRequested(
@@ -24,6 +30,23 @@ class AttendanceBloc extends Bloc<AttendanceEvent, AttendanceState> {
     result.fold(
       (failure) => emit(AttendanceFailure(message: failure.message)),
       (records) => emit(AttendanceLoaded(records: records)),
+    );
+  }
+
+  Future<void> _onScanQRCodeRequested(
+    ScanQRCodeRequested event,
+    Emitter<AttendanceState> emit,
+  ) async {
+    emit(AttendanceLoading());
+
+    final result = await scanQRCodeUseCase(ScanQRCodeParams(
+      employeeGUID: event.employeeGUID,
+      locationGUID: event.locationGUID,
+    ));
+
+    result.fold(
+      (failure) => emit(ScanQRCodeFailure(message: failure.message)),
+      (message) => emit(ScanQRCodeSuccess(message: message)),
     );
   }
 }
