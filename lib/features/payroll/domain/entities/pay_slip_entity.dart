faeddd9bc7aaf31fb3b67d0875644e868fe79f08
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-12 22:44:03 +0630

    feat: create PaySlipEntity to represent pay slip data structure with necessary fields

---
+++ lib/features/payroll/domain/entities/pay_slip_entity.dart
@@ -0,0 +1,68 @@
+import 'package:equatable/equatable.dart';
+
+class PaySlipEntity extends Equatable {
+  final String paySlipGUID;
+  final String employeeGUID;
+  final String employeeName;
+  final String departmentName;
+  final String employeeTypeName;
+  final String salaryMonth;
+  final String payPeriod;
+  final String paymentDate;
+  final int basicSalary;
+  final int totalAllowances;
+  final int totalDeductions;
+  final int netSalary;
+  final int presentDays;
+  final int lateDays;
+  final int earlyLeaveDays;
+  final int absentDays;
+  final int leaveDays;
+  final String? paySlipPath;
+  final String? currencySymbol;
+
+  const PaySlipEntity({
+    required this.paySlipGUID,
+    required this.employeeGUID,
+    required this.employeeName,
+    required this.departmentName,
+    required this.employeeTypeName,
+    required this.salaryMonth,
+    required this.payPeriod,
+    required this.paymentDate,
+    required this.basicSalary,
+    required this.totalAllowances,
+    required this.totalDeductions,
+    required this.netSalary,
+    required this.presentDays,
+    required this.lateDays,
+    required this.earlyLeaveDays,
+    required this.absentDays,
+    required this.leaveDays,
+    this.paySlipPath,
+    this.currencySymbol,
+  });
+
+  @override
+  List<Object?> get props => [
+        paySlipGUID,
+        employeeGUID,
+        employeeName,
+        departmentName,
+        employeeTypeName,
+        salaryMonth,
+        payPeriod,
+        paymentDate,
+        basicSalary,
+        totalAllowances,
+        totalDeductions,
+        netSalary,
+        presentDays,
+        lateDays,
+        earlyLeaveDays,
+        absentDays,
+        leaveDays,
+        paySlipPath,
+        currencySymbol,
+      ];
+}
