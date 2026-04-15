07b7ebeea8501566902f9a29c96789fdcf91c9b1
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-15 15:19:43 +0630

    chore: update baseUrl to 192.168.1.2 in AppConstants

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   // Use http://10.0.2.2:5000 for Android emulator
   // Use http://localhost:5000 for iOS simulator or Web
-  static const String baseUrl = 'http://192.168.1.3:9000/api';
+  static const String baseUrl = 'http://192.168.1.2:9000/api';
   static const String loginEndpoint = '/AuthApi/login';
 }
