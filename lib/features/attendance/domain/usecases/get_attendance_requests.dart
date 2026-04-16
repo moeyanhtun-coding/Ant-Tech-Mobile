93a20ac4af46103dce12e6f0f0ee64829ff6e54d
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-16 22:30:16 +0630

    feat: implement GetAttendanceRequests use case for fetching attendance requests by employee and month

---
+++ lib/features/attendance/domain/usecases/get_attendance_requests.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/error/failures.dart';
+import '../entities/attendance_request_entity.dart';
+import '../repositories/attendance_repository.dart';
+
+class GetAttendanceRequests {
+  final AttendanceRepository repository;
+
+  GetAttendanceRequests(this.repository);
+
+  Future<Either<Failure, List<AttendanceRequest>>> call({
+    required String employeeGUID,
+    required String month,
+  }) async {
+    return await repository.getAttendanceRequests(
+      employeeGUID: employeeGUID,
+      month: month,
+    );
+  }
+}
