dddbe7b0ef2c5927682e861e217626ec0e5a9948
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-17 00:41:21 +0630

    feat: add forceRefresh parameter to attendance repository methods and implement getAttendanceSummary method

--- lib/features/attendance/domain/repositories/attendance_repository.dart
+++ lib/features/attendance/domain/repositories/attendance_repository.dart
@@ -2,11 +2,13 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/error/failures.dart';
 import '../entities/attendance_entity.dart';
 import '../entities/attendance_request_entity.dart';
+import '../../domain/entities/attendance_summary_entity.dart';
 
 abstract class AttendanceRepository {
   Future<Either<Failure, List<AttendanceEntity>>> getAttendanceByEmployee({
     required String employeeGUID,
     required String month,
+    bool forceRefresh = false,
   });
 
   Future<Either<Failure, String>> scanQRCode({
@@ -19,7 +21,14 @@ abstract class AttendanceRepository {
   Future<Either<Failure, List<AttendanceRequest>>> getAttendanceRequests({
     required String employeeGUID,
     required String month,
+    bool forceRefresh = false,
   });
 
   Future<Either<Failure, String>> submitAttendanceRequest(AttendanceRequest request);
+
+  Future<Either<Failure, AttendanceSummaryEntity>> getAttendanceSummary({
+    required String employeeGUID,
+    required String month,
+    bool forceRefresh = false,
+  });
 }
