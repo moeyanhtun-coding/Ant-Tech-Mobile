8536fa289626516371389a40090d448ab326b3b8
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-16 22:30:09 +0630

    feat: extend AttendanceRepository with methods for fetching and submitting attendance requests

--- lib/features/attendance/domain/repositories/attendance_repository.dart
+++ lib/features/attendance/domain/repositories/attendance_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import '../../../../core/error/failures.dart';
 import '../entities/attendance_entity.dart';
+import '../entities/attendance_request_entity.dart';
 
 abstract class AttendanceRepository {
   Future<Either<Failure, List<AttendanceEntity>>> getAttendanceByEmployee({
@@ -14,4 +15,11 @@ abstract class AttendanceRepository {
     double? latitude,
     double? longitude,
   });
+
+  Future<Either<Failure, List<AttendanceRequest>>> getAttendanceRequests({
+    required String employeeGUID,
+    required String month,
+  });
+
+  Future<Either<Failure, String>> submitAttendanceRequest(AttendanceRequest request);
 }
