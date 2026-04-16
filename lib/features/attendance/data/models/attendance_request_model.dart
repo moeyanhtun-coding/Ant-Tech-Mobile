e7a446e4a4122c08fb042be43631119703b57c31
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-16 22:29:47 +0630

    feat: implement AttendanceRequestModel for handling attendance request data with JSON serialization

---
+++ lib/features/attendance/data/models/attendance_request_model.dart
@@ -0,0 +1,50 @@
+import '../domain/entities/attendance_request_entity.dart';
+
+class AttendanceRequestModel extends AttendanceRequest {
+  const AttendanceRequestModel({
+    required super.guid,
+    required super.employeeGUID,
+    required super.requestDate,
+    required super.requestTime,
+    super.latitude,
+    super.longitude,
+    required super.description,
+    required super.requestType,
+    required super.status,
+    super.rejectDescription,
+    super.employeeName,
+    super.departmentName,
+  });
+
+  factory AttendanceRequestModel.fromJson(Map<String, dynamic> json) {
+    return AttendanceRequestModel(
+      guid: json['guid'] ?? '',
+      employeeGUID: json['employeeGUID'] ?? '',
+      requestDate: json['requestDate'] ?? '',
+      requestTime: json['requestTime'] ?? '',
+      latitude: json['latitude'] != null ? (json['latitude'] as num).toDouble() : null,
+      longitude: json['longitude'] != null ? (json['longitude'] as num).toDouble() : null,
+      description: json['description'] ?? '',
+      requestType: json['requestType'] ?? '',
+      status: json['status'] ?? '',
+      rejectDescription: json['rejectDescription'],
+      employeeName: json['employeeName'],
+      departmentName: json['departmentName'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'guid': guid,
+      'employeeGUID': employeeGUID,
+      'requestDate': requestDate,
+      'requestTime': requestTime,
+      'latitude': latitude,
+      'longitude': longitude,
+      'description': description,
+      'requestType': requestType,
+      'status': status,
+      'rejectDescription': rejectDescription,
+    };
+  }
+}
