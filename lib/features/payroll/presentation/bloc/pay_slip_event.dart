5b8418971a2cbc2698b53f97095680d69c2154d4
moeyanhtun-coding <moeyan227970@gmail.com>
2026-04-17 23:30:13 +0630

    feat: add forceRefresh parameter to GetPaySlipsRequested event for improved data retrieval control

--- lib/features/payroll/presentation/bloc/pay_slip_event.dart
+++ lib/features/payroll/presentation/bloc/pay_slip_event.dart
@@ -10,9 +10,14 @@ abstract class PaySlipEvent extends Equatable {
 class GetPaySlipsRequested extends PaySlipEvent {
   final String employeeGUID;
   final String? month;
+  final bool forceRefresh;
 
-  const GetPaySlipsRequested({required this.employeeGUID, this.month});
+  const GetPaySlipsRequested({
+    required this.employeeGUID,
+    this.month,
+    this.forceRefresh = false,
+  });
 
   @override
-  List<Object?> get props => [employeeGUID, month];
+  List<Object?> get props => [employeeGUID, month, forceRefresh];
 }
