f469093a38cf2170152070eb94429ce47b8b08d1
moeyanhtun-coding <moeyan227970@gmail.com>
2026-05-25 20:15:59 +0630

    feat: update base URL for API connection

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   // Use http://10.0.2.2:5000 for Android emulator
   // Use http://localhost:5000 for iOS simulator or Web
-  static const String baseUrl = 'http://172.20.10.9:9000/api';
+  static const String baseUrl = 'http://13.235.51.92/api';
   static const String loginEndpoint = '/AuthApi/login';
 }
